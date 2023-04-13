13dbcee082e4d07010b663025e1b3e8ed5f2f36b
Andres Fernandez <andres94.af@gmail.con>
2023-04-13 14:47:20 +0200

    creadas las vistas de admin y sus metodos en controlador. Creada funcion para activar  inactivar clientes

--- src/main/java/com/virtualbank/controller/AdministradorController.java
+++ src/main/java/com/virtualbank/controller/AdministradorController.java
@@ -1,22 +1,79 @@
 package com.virtualbank.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.virtualbank.model.Usuario;
+import com.virtualbank.service.IMovimientosService;
+import com.virtualbank.service.IRegistroIngresoService;
+import com.virtualbank.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 
+	@Autowired
+	private IRegistroIngresoService registroIngresoService;
+
+	@Autowired
+	private IUsuarioService usuarioService;
+
+	@Autowired
+	private IMovimientosService movimientosService;
+	
 //METODO QUE REDIRECCIONA A LA VISTA PRINCIPAL DEL ADMINISTRADOR
 	@GetMapping("")
 	public String home(Model model, HttpSession session) {
-		model.addAttribute("titulo", "Hola desde home del ADMINISTRADOR");
+		model.addAttribute("titulo", "ADMINISTRADOR");
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "administrador/home_administrador";
 	}
+
+//METODO QUE REDIRECCIONA A LA VISTA DE LISTADO DE CLIENTES
+	@GetMapping("/clientes")
+	public String listadoClientes(Model model, HttpSession session) {
+		model.addAttribute("titulo", "LISTADO DE CLIENTES");
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		List<Usuario> listaDeClientes = usuarioService.findAll();
+		listaDeClientes.removeIf(c -> c.getRol().equals("ADMIN"));
+		model.addAttribute("clientes", listaDeClientes);
+		return "administrador/listado_clientes";
+	}
 	
+//METODO QUE ACTIVA/INACTIVA CLIENTES
+	@GetMapping("/clientes/activo/{id}")
+	public String activarInactivarClientes(@PathVariable Integer id) {
+		Optional<Usuario> clienteOpt = usuarioService.findById(id);
+		if (clienteOpt.isPresent()) {
+			Usuario cliente = clienteOpt.get();
+			if(cliente.isActivo()) {
+				cliente.setActivo(false);
+			}else {
+				cliente.setActivo(true);
+			}
+			usuarioService.save(cliente);
+			return "redirect:/administrador/clientes?e_ok";
+		}else{
+			System.out.println("NO EXISTEN CLIENTES CON ESTE ID");
+			return "redirect:/administrador/clientes?e_error";
+		}
+	}
+	
+//METODO QUE REDIRECCIONA A LA VISTA DE METRICAS
+	@GetMapping("/metricas")
+	public String metricas(Model model, HttpSession session) {
+		model.addAttribute("titulo", "METRICAS");
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		return "administrador/metricas";
+	}
+
 }
